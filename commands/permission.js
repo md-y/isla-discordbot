69d6b835334ba2e4cb12b016d3cac305162bbe6f
midymyth <midymyth@gmail.com>
2019-06-05 21:20:02 -0600

    Fix Permission Command

--- commands/permission.js
+++ commands/permission.js
@@ -1,6 +1,16 @@
 module.exports = {
     execute: function(msg, args, index) {
-        if (!args[0] || !index.commands[args[0]]) return;
+        const printError = function(index, msg) {
+            index.print(msg, {
+                "color": index.ERROR_COLOR,
+                "title": "Invalid/Unknown command argument."
+            });
+        }
+
+        if (!args[0]) return printError(index, msg);
+        if (index.cfg.aliases[args[0]]) args[0] = index.cfg.aliases[args[0]].split(" ")[0];
+        if (!args[0] || !index.commands[args[0]]) return printError(index, msg);
+
         let perms = index.commands[args[0]].permissions;
         let has = index.getPermission(msg, perms);
         index.print(msg, {
